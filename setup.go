3b1ce42111c62f15fac24132cc50193529c29283
1gtm <1gtm@appscode.com>
2021-07-21 18:42:42 +0000

    Generate code for provider@v1.20.1-0.20210628164239-4d5f8365936e gen@094e094
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- setup.go
+++ setup.go
@@ -28,7 +28,7 @@ import (
 	"time"
 
 	"github.com/gobuffalo/flect"
-	"github.com/hashicorp/terraform-provider-google/google"
+	google "github.com/hashicorp/terraform-provider-google/google"
 	auditlib "go.bytebuilders.dev/audit/lib"
 	arv1 "k8s.io/api/admissionregistration/v1"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"

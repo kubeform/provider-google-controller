b3d6d274b7994efad7971ef79f0a58855fd9a92b
1gtm <1gtm@appscode.com>
2021-07-22 03:34:38 +0000

    Generate code for provider@v1.20.1-0.20210628164239-4d5f8365936e gen@c270911
    Signed-off-by: 1gtm <1gtm@appscode.com>

--- setup.go
+++ setup.go
@@ -28,7 +28,7 @@ import (
 	"time"
 
 	"github.com/gobuffalo/flect"
-	"github.com/hashicorp/terraform-provider-google/google"
+	google "github.com/hashicorp/terraform-provider-google/google"
 	auditlib "go.bytebuilders.dev/audit/lib"
 	arv1 "k8s.io/api/admissionregistration/v1"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
@@ -168,8 +168,8 @@ var runningControllers = struct {
 
 func watchCRD(ctx context.Context, crdClient *clientset.Clientset, vwcClient *admissionregistrationv1.AdmissionregistrationV1Client, stopCh <-chan struct{}, mgr manager.Manager, auditor *auditlib.EventPublisher, watchOnlyDefault bool) error {
 	informerFactory := informers.NewSharedInformerFactory(crdClient, time.Second*30)
-	i := informerFactory.Apiextensions().V1beta1().CustomResourceDefinitions().Informer()
-	l := informerFactory.Apiextensions().V1beta1().CustomResourceDefinitions().Lister()
+	i := informerFactory.Apiextensions().V1().CustomResourceDefinitions().Informer()
+	l := informerFactory.Apiextensions().V1().CustomResourceDefinitions().Lister()
 
 	i.AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
@@ -194,7 +194,7 @@ func watchCRD(ctx context.Context, crdClient *clientset.Clientset, vwcClient *ad
 			if strings.Contains(crd.Spec.Group, "google.kubeform.com") {
 				gvk := schema.GroupVersionKind{
 					Group:   crd.Spec.Group,
-					Version: crd.Spec.Version,
+					Version: crd.Spec.Versions[0].Name,
 					Kind:    crd.Spec.Names.Kind,
 				}
 
